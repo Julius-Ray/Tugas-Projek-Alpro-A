deba5345cf37963b0638bd68d8d2903fdfb6ed1c
fatmawatyyunianti-tech <fatmawaty.yunianti@mhs.unsoed.ac.id>
2026-06-16 14:25:29 +0700

    Add files via upload

---
+++ |-- src_R/projek alpro program 2.R
@@ -0,0 +1,13 @@
+#PROGRAM 2
+K1 <- "Saya tak 'kan menyerah."
+K2 <- "Ia berkata, \"Aku menyayangimu.\"" #Mengandung tanda kutip ganda (") di dalam string
+# Dalam R, tambahkan \" untuk menyisipkan tanda kutip ganda di dalam string
+K3 <- "\"Coba jelaskan pengertian 'cross-validation' dalam Machine Learning!\""
+# K4 tidak ada karakter khusus yang perlu di-escape, ditulis langsung
+K4 <- "Surat keputusan itu bernomor 62/UN.34/19/2023.\njelaskan pengertian 'cross-validation'\ndalam Machine"
+
+#Output
+cat("K1:",K1,"\n")
+cat("K2:",K2,"\n")
+cat("K3:",K3,"\n")
+cat("K4:",K4,"\n")
