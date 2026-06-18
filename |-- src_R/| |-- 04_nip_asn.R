a45b9dc260d17d992887141c7084155fc94e2f10
sekarsandy <sekar.setiawan@mhs.unsoed.ac.id>
2026-06-18 11:15:16 +0700

    Update | |-- 04_nip_asn.R

--- |-- src_R/| |-- 04_nip_asn.R
+++ |-- src_R/| |-- 04_nip_asn.R
@@ -7,35 +7,38 @@ tahun<-substr(nip,1,4)
 bulan<-as.numeric(substr(nip,5,6))
 tanggal<-substr(nip,7,8)
 
-if (bulan == 1) {
-  nama_bulan <- "Januari"
-} else if (bulan == 2) {
-  nama_bulan <- "Februari"
-} else if (bulan == 3) {
-  nama_bulan <- "Maret"
-} else if (bulan == 4) {
-  nama_bulan <- "April"
-} else if (bulan == 5) {
-  nama_bulan <- "Mei"
-} else if (bulan == 6) {
-  nama_bulan <- "Juni"
-} else if (bulan == 7) {
-  nama_bulan <- "Juli"
-} else if (bulan == 8) {
-  nama_bulan <- "Agustus"
-} else if (bulan == 9) {
-  nama_bulan <- "September"
-} else if (bulan == 10) {
-  nama_bulan <- "Oktober"
-} else if (bulan == 11) {
-  nama_bulan <- "November"
-} else if (bulan == 12) {
-  nama_bulan <- "Desember"
+if (tanggal < 1 || tanggal > 31) {
+  cat("Tanggal tidak valid!\n")
 } else {
-  nama_bulan <- "Bulan tidak valid"
+  if (bulan == 1) {
+    nama_bulan <- "Januari"
+  } else if (bulan == 2) {
+    nama_bulan <- "Februari"
+  } else if (bulan == 3) {
+    nama_bulan <- "Maret"
+  } else if (bulan == 4) {
+    nama_bulan <- "April"
+  } else if (bulan == 5) {
+    nama_bulan <- "Mei"
+  } else if (bulan == 6) {
+    nama_bulan <- "Juni"
+  } else if (bulan == 7) {
+    nama_bulan <- "Juli"
+  } else if (bulan == 8) {
+    nama_bulan <- "Agustus"
+  } else if (bulan == 9) {
+    nama_bulan <- "September"
+  } else if (bulan == 10) {
+    nama_bulan <- "Oktober"
+  } else if (bulan == 11) {
+    nama_bulan <- "November"
+  } else if (bulan == 12) {
+    nama_bulan <- "Desember"
+  } else {
+    nama_bulan <- "Bulan tidak valid"
+  }
+  cat("Tanggal lahir:", tanggal, nama_bulan, tahun, "\n")
 }
-
-cat("Tanggal lahir:",tanggal, nama_bulan, tahun, "\n")
 
 
 
@@ -47,35 +50,38 @@ tahun<-substr(nip,1,4)
 bulan<-as.numeric(substr(nip,5,6))
 tanggal<-substr(nip,7,8)
 
-if (bulan == 1) {
-  nama_bulan <- "Januari"
-} else if (bulan == 2) {
-  nama_bulan <- "Februari"
-} else if (bulan == 3) {
-  nama_bulan <- "Maret"
-} else if (bulan == 4) {
-  nama_bulan <- "April"
-} else if (bulan == 5) {
-  nama_bulan <- "Mei"
-} else if (bulan == 6) {
-  nama_bulan <- "Juni"
-} else if (bulan == 7) {
-  nama_bulan <- "Juli"
-} else if (bulan == 8) {
-  nama_bulan <- "Agustus"
-} else if (bulan == 9) {
-  nama_bulan <- "September"
-} else if (bulan == 10) {
-  nama_bulan <- "Oktober"
-} else if (bulan == 11) {
-  nama_bulan <- "November"
-} else if (bulan == 12) {
-  nama_bulan <- "Desember"
+if (tanggal < 1 || tanggal > 31) {
+  cat("Tanggal tidak valid!\n")
 } else {
-  nama_bulan <- "Bulan tidak valid"
+  if (bulan == 1) {
+    nama_bulan <- "Januari"
+  } else if (bulan == 2) {
+    nama_bulan <- "Februari"
+  } else if (bulan == 3) {
+    nama_bulan <- "Maret"
+  } else if (bulan == 4) {
+    nama_bulan <- "April"
+  } else if (bulan == 5) {
+    nama_bulan <- "Mei"
+  } else if (bulan == 6) {
+    nama_bulan <- "Juni"
+  } else if (bulan == 7) {
+    nama_bulan <- "Juli"
+  } else if (bulan == 8) {
+    nama_bulan <- "Agustus"
+  } else if (bulan == 9) {
+    nama_bulan <- "September"
+  } else if (bulan == 10) {
+    nama_bulan <- "Oktober"
+  } else if (bulan == 11) {
+    nama_bulan <- "November"
+  } else if (bulan == 12) {
+    nama_bulan <- "Desember"
+  } else {
+    nama_bulan <- "Bulan tidak valid"
+  }
+  cat("Tanggal lahir:", tanggal, nama_bulan, tahun, "\n")
 }
-
-cat("Tanggal lahir:",tanggal, nama_bulan, tahun, "\n")
 
 
 #Keadaan 3
@@ -86,32 +92,35 @@ tahun<-substr(nip,1,4)
 bulan<-as.numeric(substr(nip,5,6))
 tanggal<-substr(nip,7,8)
 
-if (bulan == 1) {
-  nama_bulan <- "Januari"
-} else if (bulan == 2) {
-  nama_bulan <- "Februari"
-} else if (bulan == 3) {
-  nama_bulan <- "Maret"
-} else if (bulan == 4) {
-  nama_bulan <- "April"
-} else if (bulan == 5) {
-  nama_bulan <- "Mei"
-} else if (bulan == 6) {
-  nama_bulan <- "Juni"
-} else if (bulan == 7) {
-  nama_bulan <- "Juli"
-} else if (bulan == 8) {
-  nama_bulan <- "Agustus"
-} else if (bulan == 9) {
-  nama_bulan <- "September"
-} else if (bulan == 10) {
-  nama_bulan <- "Oktober"
-} else if (bulan == 11) {
-  nama_bulan <- "November"
-} else if (bulan == 12) {
-  nama_bulan <- "Desember"
+if (tanggal < 1 || tanggal > 31) {
+  cat("Tanggal tidak valid!\n")
 } else {
-  nama_bulan <- "Bulan tidak valid"
+  if (bulan == 1) {
+    nama_bulan <- "Januari"
+  } else if (bulan == 2) {
+    nama_bulan <- "Februari"
+  } else if (bulan == 3) {
+    nama_bulan <- "Maret"
+  } else if (bulan == 4) {
+    nama_bulan <- "April"
+  } else if (bulan == 5) {
+    nama_bulan <- "Mei"
+  } else if (bulan == 6) {
+    nama_bulan <- "Juni"
+  } else if (bulan == 7) {
+    nama_bulan <- "Juli"
+  } else if (bulan == 8) {
+    nama_bulan <- "Agustus"
+  } else if (bulan == 9) {
+    nama_bulan <- "September"
+  } else if (bulan == 10) {
+    nama_bulan <- "Oktober"
+  } else if (bulan == 11) {
+    nama_bulan <- "November"
+  } else if (bulan == 12) {
+    nama_bulan <- "Desember"
+  } else {
+    nama_bulan <- "Bulan tidak valid"
+  }
+  cat("Tanggal lahir:", tanggal, nama_bulan, tahun, "\n")
 }
-
-cat("Tanggal lahir:",tanggal, nama_bulan, tahun, "\n")
